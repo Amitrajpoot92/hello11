1a2849378016ccf764bf4882454a9448547a947f
Amitrajpoot92 <amitsrodz@gmail.com>
2026-02-03 21:25:05 +0530

    Fix import path case issue for Netlify build

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import Hero from '../components/home/hero';
 import CarAnimation from '../components/home/CarAnimation'; 
 import Fleet from '../components/home/fleet';
 import Features from '../components/home/features';
-import Trust from '../components/home/Trust'; // Naya component import kiya
+import Trust from '../components/home/trust'; // Naya component import kiya
 import Booking from '../components/home/booking';
 
 const Home = () => {
